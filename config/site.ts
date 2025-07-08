f4cdd776098c330cf7b6f1750185ed8e461796ba
besteman <ellioy37@gmail.com>
2025-07-08 15:06:54 -0400

    update site name and description

--- config/site.ts
+++ config/site.ts
@@ -1,8 +1,8 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "Next.js + HeroUI",
-  description: "Make beautiful websites regardless of your design experience.",
+  name: "Marketplace Plans",
+  description: "Make informed decisions about your health insurance.",
   navItems: [
     {
       label: "Home",
